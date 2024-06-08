5f5e1b93a0d060f2e3273c00fd6c05d0e926b70d
sbpark88 <saebyulpark88@gmail.com>
2024-06-08 18:14:49 +0900

    Feat: Redux Middleware
    - 추가 라이브러리 없이 applyMiddleware
      메소드만 사용해 적용.

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,20 @@
-import { createStore } from "redux";
+import {
+  applyMiddleware,
+  legacy_createStore as createStore,
+  Middleware,
+} from "redux";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const logger: Middleware = (store) => (next) => (action) => {
+  console.log("dispatching", action);
+  let result = next(action);
+  console.log("next state", store.getState());
+  return result;
+};
+
+const middleware = applyMiddleware(logger);
+
+const store = createStore(rootReducer, undefined, middleware);
 
 export default store;
 
