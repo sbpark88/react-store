454d5c2cc4ee9902622dab0250e94aff49d737c1
sbpark88 <saebyulpark88@gmail.com>
2024-06-10 12:08:37 +0900

    Style: Layout and CSS Styling

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Counter from "../features/counter/Counter";
+import Todo from "../features/todo/Todo";
 
 const Main: React.FC = () => {
-  return <>Page Main</>;
+  return (
+    <>
+      <Counter />
+      <Todo />
+    </>
+  );
 };
 
 export default Main;
