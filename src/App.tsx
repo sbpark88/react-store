2f793aec2443de10d7be5742895181da271f991c
sbpark88 <saebyulpark88@gmail.com>
2024-06-12 21:36:46 +0900

    Add: Todo using 'atom', 'selector'

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import TodoList from "./pages/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
