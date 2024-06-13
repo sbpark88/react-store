db0b9a6788f170c67e7633fa9d0681ae5dc9d538
sbpark88 <saebyulpark88@gmail.com>
2024-06-13 20:50:51 +0900

    Add: React-Error-Boundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React, { Suspense } from "react";
 import "./App.css";
 import TodoList from "./pages/TodoList";
 import UserInfo from "./components/UserInfo";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div className="user-info">Loading...</div>}>
-        <UserInfo />
-      </Suspense>
+      <ErrorBoundary
+        fallback={<div className="user-info">Something wrong...</div>}
+      >
+        <Suspense fallback={<div className="user-info">Loading...</div>}>
+          <UserInfo />
+        </Suspense>
+      </ErrorBoundary>
       <TodoList />
     </div>
   );
