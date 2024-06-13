744ad1fcb46eecd3d754c06757b8efb36542e4ac
sbpark88 <saebyulpark88@gmail.com>
2024-06-13 21:01:17 +0900

    Fix: 'React-Error-Boundary' to 'Loadable'

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import React, { Suspense } from "react";
+import React from "react";
 import "./App.css";
 import TodoList from "./pages/TodoList";
 import UserInfo from "./components/UserInfo";
-import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
     <div className="App">
-      <ErrorBoundary
-        fallback={<div className="user-info">Something wrong...</div>}
-      >
-        <Suspense fallback={<div className="user-info">Loading...</div>}>
-          <UserInfo />
-        </Suspense>
-      </ErrorBoundary>
+      <UserInfo />
       <TodoList />
     </div>
   );
--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,11 +1,16 @@
 import React, { useCallback } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import {
+  useRecoilState,
+  useRecoilValueLoadable,
+  useResetRecoilState,
+} from "recoil";
 import { currentUserIDState, currentUserNameState } from "../atoms/userState";
 import "./UserInfo.css";
 
 const UserInfo: React.FC = () => {
   const [userId, setUserId] = useRecoilState(currentUserIDState);
-  const userName = useRecoilValue(currentUserNameState);
+  const resetUserId = useResetRecoilState(currentUserIDState);
+  const userNameLoadable = useRecoilValueLoadable(currentUserNameState);
   const onSelectChange = useCallback(
     (event: React.ChangeEvent<HTMLSelectElement>) => {
       setUserId(parseInt(event.target.value));
@@ -13,26 +18,41 @@ const UserInfo: React.FC = () => {
     [setUserId],
   );
 
-  return (
-    <>
-      <div className="user-info">
-        <label>사용자 ID:</label>
-        <select className="user--id" value={userId} onChange={onSelectChange}>
-          {Array(11)
-            .fill("")
-            .map((_, index) => (
-              <option key={index + 1} value={index + 1}>
-                {index + 1}
-              </option>
-            ))}
-        </select>
-        <div className="user--name">{userName}</div>
-      </div>
-      <p style={{ textAlign: "right", margin: 0 }}>
-        User ID '11' will be throw asynchronous error
-      </p>
-    </>
-  );
+  switch (userNameLoadable.state) {
+    case "loading":
+      return <div className="user-info">Loading...</div>;
+    case "hasError":
+      return (
+        <div className="user-info">
+          Something wrong...<button onClick={resetUserId}>Reset</button>
+        </div>
+      );
+    case "hasValue":
+      return (
+        <>
+          <div className="user-info">
+            <label>사용자 ID:</label>
+            <select
+              className="user--id"
+              value={userId}
+              onChange={onSelectChange}
+            >
+              {Array(11)
+                .fill("")
+                .map((_, index) => (
+                  <option key={index + 1} value={index + 1}>
+                    {index + 1}
+                  </option>
+                ))}
+            </select>
+            <div className="user--name">{userNameLoadable.contents}</div>
+          </div>
+          <p style={{ textAlign: "right", margin: 0 }}>
+            User ID '11' will be throw asynchronous error
+          </p>
+        </>
+      );
+  }
 };
 
 export default UserInfo;
