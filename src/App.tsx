79c4550bb76a531fb22ac2630dc0b31daabae027
sbpark88 <saebyulpark88@gmail.com>
2024-06-13 21:12:46 +0900

    Revert "Add: Asynchronous selector"
    This reverts commit 4e56cb12219d7d24256a5a30d2dd015c7a2cda61.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React, { Suspense } from "react";
+import React from "react";
 import "./App.css";
 import TodoList from "./pages/TodoList";
-import UserInfo from "./components/UserInfo";
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div className="user-info">Loading...</div>}>
-        <UserInfo />
-      </Suspense>
       <TodoList />
     </div>
   );
