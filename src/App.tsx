f5e47f28069197a0eb53a7549789df738dfa598c
sbpark88 <saebyulpark88@gmail.com>
2024-06-13 21:12:46 +0900

    Revert "Fix: 'React-Error-Boundary' to 'Loadable'"
    This reverts commit 744ad1fcb46eecd3d754c06757b8efb36542e4ac.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import TodoList from "./pages/TodoList";
 import UserInfo from "./components/UserInfo";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
     <div className="App">
-      <UserInfo />
+      <ErrorBoundary
+        fallback={<div className="user-info">Something wrong...</div>}
+      >
+        <Suspense fallback={<div className="user-info">Loading...</div>}>
+          <UserInfo />
+        </Suspense>
+      </ErrorBoundary>
       <TodoList />
     </div>
   );
