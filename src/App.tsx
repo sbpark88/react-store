de442f61fdfa6384242b7082ed084d3c1e71cfbc
sbpark88 <saebyulpark88@gmail.com>
2024-06-14 23:18:58 +0900

    Revert "Add: Asynchronous Actions"
    This reverts commit 9450c3c4cf45a22657f748ab906a680149fe1177.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import "./App.css";
 import Counter from "./features/counter/Counter";
 import Todo from "./features/todo/Todo";
-import UserInfo from "./features/user/UserInfo";
 
 function App() {
   return (
     <div className="App">
-      <UserInfo />
       <Counter />
       <Todo />
     </div>
